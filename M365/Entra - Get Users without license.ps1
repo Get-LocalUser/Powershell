4dd88f22fd32660213d3ef42e569d8ac84e77586
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2024-11-01 11:34:38 -0700

    .

--- M365/Entra - Get Users without license.ps1
+++ M365/Entra - Get Users without license.ps1
@@ -1,3 +1,16 @@
+<#
+=============================================================================================
+Name:           Entra - Get Users Without Licenses
+Description:    Retrieves users in the tenant that have no licenses
+Prerequisites:  EntraID Module
+Other:          Slight adjustment from MS's script https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.entra/get-entrauser?view=entra-powershell
+
+Script Tasks: 
+~~~~~~~~~~~~~~~~~
+============================================================================================
+#>
+
+
 Connect-Entra -Scopes 'User.Read.All'
 $EntraUsers = Get-EntraUser -All
 $EntraUsersWithLicenses = foreach ($user in $EntraUsers) {
