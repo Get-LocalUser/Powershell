532ef5de1eff935ef5291bdbbf368436ffac0425
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2024-12-13 11:42:45 -0800

    formating change

--- M365/Entra - Get Users without license.ps1
+++ M365/Entra - Get Users without license.ps1
@@ -16,10 +16,10 @@ $EntraUsers = Get-EntraUser -All
 $EntraUsersWithLicenses = foreach ($user in $EntraUsers) {
     if ($user.AssignedLicenses.Count -lt 1) {
         [pscustomobject]@{
-            Id               = $user.Id
-            DisplayName      = $user.DisplayName
-            UserPrincipalName = $user.UserPrincipalName
-            AssignedLicenses = ($user.AssignedLicenses | ForEach-Object { $_.SkuId }) -join ", "
+            Id                  = $user.Id
+            DisplayName         = $user.DisplayName
+            UserPrincipalName   = $user.UserPrincipalName
+            AssignedLicenses    = ($user.AssignedLicenses | ForEach-Object { $_.SkuId }) -join ", "
         }
     }
 }
