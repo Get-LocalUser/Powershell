4125f16f577137375bbd2b05e043cfa06ed8c48f
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2025-05-07 18:07:26 -0700

    .

--- M365/Intune - Check & Remove Intune Devices.ps1
+++ M365/Intune - Check & Remove Intune Devices.ps1
@@ -1,60 +1,58 @@
-# Check of Graph Beta module is installed
+
+
+# Check if Graph Beta module is installed
 $RequiredModule = Get-InstalledModule -Name Microsoft.Graph.Beta
 if (!$RequiredModule) {
-        Install-Module -Name Microsoft.Graph.Beta
-        Import-Module -Name Microsoft.Graph.Beta
-    }
+    Install-Module -Name Microsoft.Graph.Beta
+}
+Import-Module -Name Microsoft.Graph.Beta
 
 # Connect to Graph
-Connect-MgGraph -Scopes " Device.Read.All, Directory.ReadWrite.All, Directory.Read.All, Device.ReadWrite.All,"
+Connect-MgGraph -Scopes "Device.Read.All, Directory.ReadWrite.All, Directory.Read.All, Device.ReadWrite.All,"
 
 # Define CSV file
 $CsvFile = Read-Host "Please enter the path to your CSV file"
-
 $ImportedCsv = Import-Csv -Path $CsvFile
 
-# Iterate over the serial numbers in the CSV file
+# Create a hashtable to store lookup results
+$deviceLookup = @{}
+
+# Iterate over the asset tags in the CSV file
 foreach ($row in $ImportedCSV) {
-    $AssetTag = $row.AssetTag
+    $assetTag = $row.AssetTag
 
-
-    if ([string]::IsNullOrWhiteSpace($AssetTag)) {
+    if ([string]::IsNullOrWhiteSpace($assetTag)) {
         Write-Host "Empty asset tag - skipping..."
         continue
     }
 
-    # Get the devices in the Autopilot service
-    $Devices = Get-MgBetaDevice | Where-Object {$_.DisplayName -eq $AssetTag}
+    # Get the devices in Intune
+    $devices = Get-MgBetaDeviceManagementManagedDevice | Where-Object {$_.DeviceName -eq $assetTag}
+    $deviceLookup[$assetTag] = $devices
 
-    if ($Devices) {
-        Write-Host "Device '$AssetTag' FOUND in Intune." -ForegroundColor Green
+    if ($devices) {
+        Write-Host "Device '$assetTag' FOUND in Intune." -ForegroundColor Green
     } else {
-    Write-Host "Device(s) '$AssetTag' NOT FOUND in Intune." -ForegroundColor Red
+        Write-Host "Device '$assetTag' NOT FOUND in Intune." -ForegroundColor Red
     }
-} 
+}
 
-$question = Read-Host "Would you like to remove the device(s) from Intune? (Y/N)"
+$question = Read-Host "Would you like to remove these devices from Intune? (Y/N)"
 if ($question -eq "Y") {
-    foreach ($row in $ImportedCSV) {
-        $AssetTag = $row.AssetTag
-        if ([string]::IsNullOrWhiteSpace($AssetTag)) {
-            continue
-        }
-        
-        # Get the device identity object that matches the serial number
-        $deviceToRemove = Get-MgBetaDevice | Where-Object {$_.DisplayName -eq $AssetTag}
+    foreach ($assetTag in $deviceLookup.Keys) {
+        $devices = $deviceLookup[$assetTag]
         
         # Then remove each matching device by its ID
-        if ($deviceToRemove) {
-            foreach ($device in $deviceToRemove) {
-                Write-Host "Removing device '$AssetTag'..." -ForegroundColor Yellow
-                Remove-MgBetaDevice -DeviceId $device.Id
-                Write-Host "Device removed successfully." -ForegroundColor Green
+        if ($devices) {
+            foreach ($device in $devices) {
+                Write-Host "Removing device with asset tag '$assetTag'..." -ForegroundColor Yellow
+                Remove-MgBetaDeviceManagementManagedDevice -ManagedDeviceId $device.Id
+                Write-Host "Device with asset tag '$assetTag' removed successfully." -ForegroundColor Green
             }
         } else {
-            Write-Host "No device found with name '$AssetTag'. Skipping." -ForegroundColor Yellow
+            Write-Host "No devices found with asset tag '$assetTag'. Skipping." -ForegroundColor Yellow
         }
     }
 }
 
-Write-Host "Don't forget to sign out of the graph!." -ForegroundColor Cyan
+Write-Host "Don't forget to sign out of the graph! using 'Disconnect-MgGraph'" -ForegroundColor Cyan
