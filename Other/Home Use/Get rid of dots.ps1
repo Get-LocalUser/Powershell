a0c9d9a4cd7697b02b7878c16a83d0b8a73f019c
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2025-07-02 06:13:39 -0700

    Delete Other/Home Use directory

--- Other/Home Use/Get rid of dots.ps1
+++
@@ -1,7 +0,0 @@
-$folder = "C:\Users\harison\Downloads\To Move"
-
-Get-ChildItem $folder | ForEach-Object {
-    $newname = $_.Name -replace '\.', ' '  # Modify only the file name
-    $newpath = Join-Path $_.DirectoryName $newname  # Create the new full path
-    Rename-Item -Path $_.FullName -NewName $newpath
-}
