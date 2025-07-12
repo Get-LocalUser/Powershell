2b6139109a5e7420d3dbd7dd9c222b07ec3eec6a
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2025-07-11 22:50:42 -0700

    Update Maintenance - Empty Local Temp Selection.ps1

--- Maintenance/Maintenance - Empty Local Temp Selection.ps1
+++ Maintenance/Maintenance - Empty Local Temp Selection.ps1
@@ -1,19 +1,18 @@
-
 $accountfolders = Get-ChildItem -Path "C:\Users"
-$join = @(Join-Path $accountfolders.FullName "AppData\Local\Temp")
 
 Write-host "Accounts on the system are.." -ForegroundColor Yellow
 foreach ($account in $accountfolders) {
     Write-Host $account.Name -ForegroundColor Magenta
 } 
 
-$question = Read-Host "Which account whould you'd like to have the App Data erased for?"
+$question = Read-Host "Which account would you like to have the App Data erased for?"
 
 foreach ($account in $accountfolders) {
     if ($question -eq $account.Name) {
+        $tempPath = Join-Path $account.FullName "AppData\Local\Temp"
         try {
-        Remove-Item -Path $join -Recurse -ErrorAction SilentlyContinue 
-        Write-Host "Temp contents deleted from $($account.Name)" -ForegroundColor Green
+            Remove-Item -Path $tempPath -Recurse -Force -ErrorAction SilentlyContinue
+            Write-Host "Temp contents deleted from $($account.Name)" -ForegroundColor Green
         }
         catch {
             Write-Error $_
